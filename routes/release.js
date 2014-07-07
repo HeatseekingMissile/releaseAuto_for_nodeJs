c57679d63cd24fcc3613553f170eee8103b6b4eb
eucitaht <testlushi@163.com>
2014-07-07 16:31:13 +0800

    update ui
    update ui

--- routes/release.js
+++ routes/release.js
@@ -7,15 +7,14 @@ var formidable = require("formidable");
 //shell
 var spawn = require('child_process').spawn;
 
-
-
+var STATUS_NO_THROUGH = false;
+var STATUS_THROUGH = true;
 var config = {
   user:'',
   password:'',
   host:'',
   database:''
 };
-
 var client = mysql.createConnection(config);
 client.connect(function(error,results){
     if(error){
@@ -38,14 +37,14 @@ var mysqlCrud = {
             }
         });
     },
-    update:function(client,sql){
-        client.query(sql,function(err,result){
+    update:function(client,sql,value,res){
+        client.query(sql,value,function(err,result){
             if(err){
               console.log("error -> "+err.message);
               client.end();
             }else{
               console.log("update success");
-              // res.send('update success sql -> '+sql);
+              res.send(STATUS_THROUGH);
             }
         });
     },
@@ -75,8 +74,9 @@ var mysqlCrud = {
 };
 
 //crud sql...
-var addReleaseSql="insert into releases set projectType=?,projectHost=?,projectContent=?,isCreateReleaseFile=?,isCopyOriginalFile=?,isResetTomcat=?";
+var addReleaseSql="insert into releases set projectType=?,projectHost=?,projectContent=?,position=?,isCreateReleaseFile=?,isCopyOriginalFile=?,isResetTomcat=?,status=?,fileHosts=?,ipHost=?";
 var findReleaseSql="select * from releases";
+var updateStatusByIdSql = "update releases set status='1' where id=?";
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send('release with a resource');
@@ -96,8 +96,9 @@ router.post('/add',function(req,res){
     form.parse(req, function(err, fields, files) {
       var protos = [];
       for(var key in fields){
-          if(fields.hasOwnProperty(key)){
+          if(fields.hasOwnProperty(key)&&key!='upfile'){
               protos.push(fields[key]);
+              console.log('key -> ' + key + ' fields[key] -> ' + fields[key]);
           };
       }
       var tmp_path = files.file1.path;
@@ -110,16 +111,33 @@ router.post('/add',function(req,res){
            console.log('File uploaded to: ' + target_path + ' - ' + files.file1.size + ' bytes');
          });
       });
-      // console.log(protos);
-      // console.log(files);
+      protos.push(STATUS_NO_THROUGH);
+      protos.push(target_path);
+      protos.push(req.connection.remoteAddress);
+      mysqlCrud.insert(client,addReleaseSql,protos);
     });
 
-    // mysqlCrud.insert(client,addReleaseSql,protos);
+
     // var free  = spawn('ps', ['-ef']);//执行shell
     // free.stdout.on('data', function (data) {
     //     console.log('脚本显示结果：\n' + data);
     // });
-    res.send('发布成功 等待测试通过');
+    // res.send('发布成功 等待测试通过');
+    release={
+        title: 'Release for NodeJs',
+        listName:'发布流程',
+        uploadFile:'上传文件',
+        technologicalProcess:['上传文件-upload','测试验证-veriFication','手动发布-autoRelease'],
+        submitName:'上传'
+    }
+    res.render('index',release);
+});
+
+router.post('/updateStatus',function(req,res){
+    var releaseId = req.body.releaseId;
+
+    mysqlCrud.update(client,updateStatusByIdSql,releaseId,res);
+
 });
 
 module.exports = router;
